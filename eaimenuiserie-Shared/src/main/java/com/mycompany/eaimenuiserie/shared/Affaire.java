e1dfee71d0df5004dcf44483e293b38732e01bdc
GeoffreySaintGermain <geoffrey40360@hotmail.fr>
2021-11-26 11:45:48 +0100

    add shared for model

---
+++ eaimenuiserie-Shared/src/main/java/com/mycompany/eaimenuiserie/shared/Affaire.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.eaimenuiserie.shared;
+
+import java.util.UUID;
+
+/**
+ *
+ * @author GeoffreySaint-Germai
+ */
+public class Affaire {
+    
+    private UUID identite;
+    private String rue;
+    private String ville;
+    private String cp;
+    private String mail;
+    private String telephone;
+    private String entreprise;
+    private String lieuPose;
+    private statutAffaire statut;
+    
+    public Affaire(String rue, String ville, String cp, String mail, 
+            String telephone, String entreprise, String lieuPose) {
+        this.rue = rue;
+        this.ville = ville;
+        this.cp = cp;
+        this.mail = mail;
+        this.telephone = telephone;
+        this.entreprise = entreprise;
+        this.lieuPose = lieuPose;
+        
+        this.statut = statutAffaire.ENCOURS;
+        this.identite = UUID.randomUUID();
+    }
+    
+    enum statutAffaire {
+        ENCOURS,
+        FERME
+    }
+}
---
+++ eaimenuiserie-Shared/src/main/java/com/mycompany/eaimenuiserie/shared/Commande.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.eaimenuiserie.shared;
+
+import java.util.UUID;
+
+/**
+ *
+ * @author GeoffreySaint-Germai
+ */
+public class Commande {
+    
+    private UUID identite;
+    private String refCatalogueProduit;
+    private Mesure mesure;
+    
+    public Commande(String refCatalogueProduit, Mesure mesure) {
+        this.refCatalogueProduit = refCatalogueProduit;
+        this.mesure = mesure;
+        
+        this.identite = UUID.randomUUID();
+    }
+
+    class Mesure {
+        
+        private String mesureLargeur;
+        private String mesureLongueur;
+        
+        public Mesure(String mesureLargeur, String mesureLongueur) {
+            this.mesureLargeur = mesureLargeur;
+            this.mesureLongueur = mesureLongueur;
+            
+                    
+        }
+    }
+}
